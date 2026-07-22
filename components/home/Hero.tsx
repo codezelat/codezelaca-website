8b1fa4406e3372c84b9f117a35f1ae27c26e42ba
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2026-07-22 07:12:27 +0530

    Optimize hero image delivery

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -76,13 +76,13 @@ export function Hero() {
           <div className="relative h-[262px] min-w-0 lg:h-[514px] lg:p-[10px]">
             <div className="absolute inset-x-0 bottom-0 h-[322px] lg:inset-[10px] lg:h-auto">
               <Image
-                src="/images/cca/Hero-BG.png"
+                src="/images/cca/Hero-BG.webp"
                 alt="Learner celebrating while holding a laptop, surrounded by technology icons"
                 fill
                 preload
                 fetchPriority="high"
-                quality={90}
                 sizes="(min-width: 1024px) 610px, calc(100vw - 20px)"
+                unoptimized
                 className="object-cover object-bottom"
               />
             </div>
